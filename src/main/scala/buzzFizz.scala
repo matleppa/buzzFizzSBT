31f10364833878e87d7fda0abd3e3a405ed1fdfa
Matti Leppänen <matti.leppanen@apinf.io>
2018-10-14 18:43:24 +0300

    Added comments

--- src/main/scala/buzzFizz.scala
+++ src/main/scala/buzzFizz.scala
@@ -1,16 +1,25 @@
 object buzzFizz extends App {
   def buzzFizz(L: Seq[Int]): Seq[String] = {
+    // initiate the response array of strings
     var vastaus = new Array[String](0)
 
+    // handle incoming array in loop
     for (x <- L) {
+      // initiate variable for possible Fizz or Buzz text
       var text: String = ""
+
+      // if divisible by 3, text needed for replacement
       if (x % 3 == 0) {
         text = "Buzz"
       }
+
+      // if divisible by 5, text needed for replacement
+      // note: might have been also divisible by 3, so let's append this text
       if (x % 5 == 0) {
         text += "Fizz"
       }
 
+      // append to response array either replacement text or the number
       if (text.length > 0) {
         vastaus :+= text
       } else {
@@ -18,6 +27,7 @@ object buzzFizz extends App {
       }
     }
 
+    // return response array
     vastaus
   }
 }
